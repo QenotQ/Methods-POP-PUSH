5bd912ab8192c064c1284c80166731422f9d76b3
QenotQ <100340949+QenotQ@users.noreply.github.com>
2022-03-18 22:23:48 +0300

    Add files via upload

---
+++ Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._03._22
+{
+    internal class Methods
+    {
+            public static int Pop(ref int[] array)
+            {
+            int deleted = array[array.Length - 1];
+            int[] newArray = new int[array.Length - 1];
+
+            for (int i = 0; i < newArray.GetLength(0); i++)
+            {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+            return deleted;
+             }
+
+            public static int Push(ref int[] array)
+            {
+            int[] newArray = new int[array.Length + 1];
+            Random random = new Random();
+
+            for (int i = 0; i < newArray.GetLength(0); i++)
+            {
+                if (i < newArray.Length - 1)
+                newArray[i] = array[i];
+                else
+                newArray[i] = random.Next(8); ;
+            }
+            array = newArray;
+           
+            return newArray.Length;
+            }
+        }
+    }
+
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using _18._03._22;
+int num = 5;
+int[] array = new int[num];
+Random random = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+    array[i] = random.Next(8);
+
+Console.Write("введённый массив ");
+Console.WriteLine(string.Join(" ", array));
+int lenght = Methods.Push(ref array);
+Console.WriteLine("Длинна изменённого массива " + lenght);
+Console.Write("Изменённый массив ");
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine();
+Console.Write("введённый массив ");
+Console.WriteLine(string.Join(" ", array));
+int deleted = Methods.Pop(ref array);
+Console.WriteLine("Удалённый элемент массива " + deleted);
+Console.Write("Изменённый массив ");
+Console.WriteLine(string.Join(" ", array));
